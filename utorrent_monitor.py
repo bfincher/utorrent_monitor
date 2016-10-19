83263a12c048a49aea83b7e5ee128e67d74abe3b
Brian Fincher <brian@fincherhome.com>
2016-10-18 21:28:55 -0500

    allow for case when no data was received from client

--- utorrent_monitor.py
+++ utorrent_monitor.py
@@ -26,54 +26,61 @@ class UtorrentMonitor(object):
         self.client = UtorrentClient(host, username, password)
 
     def run(self):
-        self.client.list()
+        try:
+            self.client.list()
+
+            if not self.client.response:
+                logger.warn('No data received from client')
+                return
         
-        #print 'response = %s' % self.client.response.text
-        j = json.loads(self.client.response.text)
+            #print 'response = %s' % self.client.response.text
+            j = json.loads(self.client.response.text)
 
-        now = datetime.now()
-        allHashes = []
-        for entry in j['torrents']:
-            tHash = entry[0]
-            allHashes.append(tHash)
-            title = entry[2]
-            label = entry[11]
-            status = entry[21]
-            #added = datetime.fromtimestamp(entry[23])
-            completed = datetime.fromtimestamp(entry[24])
+            now = datetime.now()
+            allHashes = []
+            for entry in j['torrents']:
+                tHash = entry[0]
+                allHashes.append(tHash)
+                title = entry[2]
+                label = entry[11]
+                status = entry[21]
+                #added = datetime.fromtimestamp(entry[23])
+                completed = datetime.fromtimestamp(entry[24])
 
-            defaults={'name': title, 'status': status, 'label': label, 'emailed': False}
-            entry, created = CompletedTorrents.objects.get_or_create(
-                hash=tHash,
-                defaults=defaults)
+                defaults={'name': title, 'status': status, 'label': label, 'emailed': False}
+                entry, created = CompletedTorrents.objects.get_or_create(
+                    hash=tHash,
+                    defaults=defaults)
 
-            if created:
-                logger.info('Created DB entry.  %s, %s', tHash, title)
+                if created:
+                    logger.info('Created DB entry.  %s, %s', tHash, title)
 
-            if entry.status != status or entry.label != label:
-                entry.status = status
-                entry.label = label
-                entry.save()
-
-            if (not entry.emailed) and (status == 'Finished' or status == 'Seeding'):
-                send_email(title)
-                entry.emailed = True
-                entry.save()
-            
-            if (label in LABELS_TO_DELETE and status in STATUS_TO_DELETE):
-                delta = now - completed
-                if (delta > REQUIRED_COMPLETION_TIME):
-                    if logger.isEnabledFor(DEBUG):
-                        logger.debug('%s %s %s %s %s %s %s', tHash, title, label, status, added, completed, delta.total_seconds())
-                    
-                    logger.info('deleting %s', title)
-                    self.client.removeData(tHash)
+                if entry.status != status or entry.label != label:
+                    entry.status = status
+                    entry.label = label
                     entry.save()
 
-        #delete entris that are not in the current list of data from Utorrent
-        for toDelete in CompletedTorrents.objects.exclude(hash__in=allHashes):
-            logger.info('Deleting DB entry %s, %s', toDelete.hash, toDelete.name)
-            toDelete.delete()
+                if (not entry.emailed) and (status == 'Finished' or status == 'Seeding'):
+                    send_email(title)
+                    entry.emailed = True
+                    entry.save()
+            
+                if (label in LABELS_TO_DELETE and status in STATUS_TO_DELETE):
+                    delta = now - completed
+                    if (delta > REQUIRED_COMPLETION_TIME):
+                        if logger.isEnabledFor(DEBUG):
+                            logger.debug('%s %s %s %s %s %s %s', tHash, title, label, status, added, completed, delta.total_seconds())
+                    
+                        logger.info('deleting %s', title)
+                        self.client.removeData(tHash)
+                        entry.save()
+
+            #delete entris that are not in the current list of data from Utorrent
+            for toDelete in CompletedTorrents.objects.exclude(hash__in=allHashes):
+                logger.info('Deleting DB entry %s, %s', toDelete.hash, toDelete.name)
+                toDelete.delete()
+        except Exception, e:
+            logger.exception(e)
 
 def send_email(content):
     msg = MIMEText('Torrent %s completed' % content)
