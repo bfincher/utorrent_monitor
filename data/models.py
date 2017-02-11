ba48b794d0e3ded1db304ea6a8ffa4afb6bd2f13
Brian Fincher <brian@fincherhome.com>
2017-02-10 20:38:02 -0600

    add startseedingtime

--- data/models.py
+++ data/models.py
@@ -6,6 +6,7 @@ class CompletedTorrents(models.Model):
     label = models.CharField(max_length=20, blank=True)
     status = models.CharField(max_length=20)
     emailed = models.BooleanField(default=False)
+    startSeedingTime = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return 'hash = %s, name = %s, label = %s, status = %s, emailed = %s' % (self.hash, self.name, self.label, self.status, self.emailed)
